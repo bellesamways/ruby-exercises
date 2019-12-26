007bbdada0f5360b0c24f7b07e4db1b0438d88db
Isabelle Samways <belesamways@gmail.com>
2019-12-25 23:12:19 -0300

    leitura e escrita de arquivos

---
+++ read.rb
@@ -0,0 +1,5 @@
+file = File.open('list.txt')
+
+file.each do |line|
+  puts line
+end
---
+++ write.rb
@@ -0,0 +1,7 @@
+File.open('list.txt', 'a') do |line|
+  line.puts('arroz')
+  line.puts('feijão')
+  line.print('azeite')
+  line.print('de')
+  line.print('oliva')
+end
